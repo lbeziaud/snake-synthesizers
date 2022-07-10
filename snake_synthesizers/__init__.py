58086e62c91c35e35fcdbb924adc00b1cf9f494d
Louis Béziaud <louis.beziaud@irisa.fr>
2022-07-10 14:13:48 -0400

    first commit

---
+++ snake_synthesizers/__init__.py
@@ -0,0 +1,6 @@
+from snake_synthesizers.copulashirley import MyCopulaShirley
+from snake_synthesizers.mst import MyMST
+from snake_synthesizers.pategan import MyPATEGAN
+from snake_synthesizers.privbayes import MyPrivBayes
+
+__all__ = ["MyCopulaShirley", "MyMST", "MyPATEGAN", "MyPrivBayes"]
